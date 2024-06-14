fd13375c552b1709bc1193f1a7e1e6e945814dc8
Sumant Kumar Pandey <96868383+sumantP2002@users.noreply.github.com>
2024-06-14 08:01:38 +0530

    register user contrller

--- backendsetup/src/app.js
+++ backendsetup/src/app.js
@@ -8,26 +8,23 @@ const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN
 }));
-
 //config for json data that come to backend and setting the limit tp save our 
 app.use(express.json({
     limit: "16kb"
 }))
-
 //config when data come as url 
 app.use(express.urlencoded({extended: true}))
-
 //cookie-parser is for allowing server to access the cookies on client system and perform crud operating
 app.use(cookieParser());
-
 // sometime i want to store private file or folder on our local system so create public folder
 app.use(express.static("public")); 
 
 
 //routes import
 import userRouter from './routes/user.routes.js'
+
 //routes declaration -> as we have placed route outside so get will not work we have to use 'use' middleware
-app.use("api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 
 
 export { app } 
--- backendsetup/src/controllers/user.controller.js
+++ backendsetup/src/controllers/user.controller.js
@@ -2,10 +2,18 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
-    console.log("i am here");
+    //it is a challenge so first steps
+    //1. get user fill details from frontend
+    //2. Validation(not empty)
+    //3. check if already exist (username, email)
+    //4. check for img, check for avatar
+    //5. upload them to cloudanary, avatar
+    //6. create user object -> create entry in db
+    //7. remove encripted password and refresh token feild from response
+    //8. check if response came or not
+    //9. return res
+
+    
 })
 
 
